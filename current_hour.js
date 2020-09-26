ed9134ad58dac1521b049617a3578eb7713f9715
Emre <emrekurtt1655@gmail.com>
2020-09-27 00:29:26 +0200

    feat: convert time to 12 hours format

--- current_hour.js
+++ current_hour.js
@@ -11,12 +11,18 @@ getmyhour.addEventListener("click", utchour)
 function utchour() {
   var today = new Date();
   var h = today.getHours();
+  // in-order to convert it to 12 hours format you can take % 12 on the current time.
+  // If the time is 13 then 13%12 → 1
+  // time = 23 then 23%12 →11
+  // time = 24, then 24%12 → 0 , if the time is 0, then change the time as 12.
+  var AmOrPm = h >= 12 ? 'pm' : 'am';
+  h = (h % 12) || 12; 
   var m = today.getMinutes();
   var s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
   h = checkTime(h);
-  document.getElementById("date").innerHTML = h + ":" + m + ":" + s;
+  document.getElementById("date").innerHTML = h + ":" + m + ":" + s + " " + AmOrPm;
   var t = setTimeout(utchour, 1000);
 }
 function checkTime(i) {
